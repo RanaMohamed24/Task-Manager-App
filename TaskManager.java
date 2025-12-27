6cdeea8a10f1afafa557e06da5d49dc59afb5d68
RanaMohamed24 <raanaamohamed695@gmail.com>
2025-12-27 14:01:34 +0200

    Add TaskManager.java

---
+++ TaskManager.java
@@ -0,0 +1,8 @@
+public class TaskManager{
+     public static void addTask(String task){
+           system.out.println("task added: "+task);
+     }
+    public static void main(String[] args){
+      addTask("Study Git ")
+    }
+}
